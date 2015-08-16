6b68e9b2919431cedceab7ed3cd2279f64db0993
Austin Worachet <wworachet@gmail.com>
2015-08-16 10:03:04 -0700

    answerCtrl cleanup

--- client/app/answerCtrl.js
+++ client/app/answerCtrl.js
@@ -4,23 +4,10 @@ angular.module('answerCtrl', [])
 
     var vm = this;
     vm.answers = [];
-    vm.answersArr = [];
-    vm.Total = 0;
 
     // use Admin factory to make ajax request to get all answers
     Admin.getAnswers().then(function (response) {
       vm.answers = angular.copy(response);
-      vm.answersArr.forEach(function (a) {
-      })
     });
-
-    // show the number of responses for each answer
-    vm.getTotal = function () {
-      for (var i = 0; i < vm.answer.answers.length; i++) {
-        var answer = vm.answer.answers[i];
-        total += answer.answer_count;
-      }
-      return total;
-    };
 
   }]);
