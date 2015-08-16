861839f920a67bcc2e020384772855f1e0f5c0c2
Austin Worachet <wworachet@gmail.com>
2015-08-16 09:50:33 -0700

    gruntfile cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-karma');
-  // grunt.loadNpmTasks('grunt-contrib-less');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     jshint: {
@@ -11,62 +8,12 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       },
       files: ['Gruntfile.js', 'server/**/*.js','client/app/**/*.js']
-    },
-
-    // karma: {
-    //   unit: {
-    //     configFile: 'specs/karma-unit.config.js',
-    //     autoWatch: false,
-    //     singleRun: true
-    //   },
-    //   midway: {
-    //     configFile: 'specs/karma-midway.config.js',
-    //     autoWatch: false,
-    //     singleRun: true
-    //   },
-
-      // endtoend: {
-      //   configFile: 'specs/karma-e2e.config.js',
-      //   autoWatch: false,
-      //   singleRun: true
-      // }
-    // },
-
-    watch: {
-      stylesheets: {
-        files: 'client/assets/**/*.less',
-        tasks: ['less:production']
-      }
-    // },
-
-    // less: {
-    //   development: {
-    //     options: {
-    //       paths: ["client/assets/css"]
-    //     },
-    //     files: {
-    //       "client/assets/css/styles.css": "client/assets/less/*.less"
-    //     }
-    //   },
-    //   production: {
-    //     options: {
-    //       paths: ["client/assets/css"]
-    //     },
-    //     files: {
-    //        "client/assets/css/styles.css": "client/assets/less/*.less"
-    //     }
-    //   }
     }
   });
 
-
-
   grunt.registerTask('test', [
     'jshint'
-    // 'karma'
     ]);
-  // grunt.registerTask('unit', [
-  //   'karma:'])
 
   grunt.registerTask('default', 'test');
 
